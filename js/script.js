ebd69aed2b7ce84daf79b9b9d65aa884e902ce5a
Bartosz Janicki <janicek2@gmail.com>
2018-06-24 14:46:46 +0200

    Add scripts

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
+//Task 1
+
 const first = 'Hello';
-const secend = 'Word';
+const second = 'Word';
 
 const add = `${first} + ${second}`;
+
+//Task 2
+
+const multiply = (a ='2',b ='5') => a * b;
+const multiply1 = (x ='8') =>  x * 1 ;
+
+//Task 3
+
+const average = (...rest) => rest.reduce( (a,b) => a+b) / rest.length;
+average(1);
+average(1, 3);
+average(1, 3, 6, 6);
+
+//Task 4
+
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+const average2 = (arr) => arr.reduce( (a,b) => a+b) / arr.length;
+average2(grades);
+
+//Task 5
+
+const arrayone = [1, 4, 'Iwona', false, 'Nowak'];
+const [, , firstname, , lastname] = arrayone;
+
+
+
+
 
--- script-compiled.js
+++ script-compiled.js
@@ -1,7 +1,51 @@
 'use strict';
 
+//Task 1
+
 var first = 'Hello';
 var second = 'Word';
 
 var add = first + ' + ' + second;
 
+//Task 2
+
+var multiply = function multiply() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '2';
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '5';
+  return a * b;
+};
+var multiply1 = function multiply1() {
+  var x = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '8';
+  return x * 1;
+};
+
+//Task 3
+
+var average = function average() {
+  for (var _len = arguments.length, rest = Array(_len), _key = 0; _key < _len; _key++) {
+    rest[_key] = arguments[_key];
+  }
+
+  return rest.reduce(function (a, b) {
+    return a + b;
+  }) / rest.length;
+};
+average(1);
+average(1, 3);
+average(1, 3, 6, 6);
+
+//Task 4
+
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+var average2 = function average2(arr) {
+  return arr.reduce(function (a, b) {
+    return a + b;
+  }) / arr.length;
+};
+average2(grades);
+
+//Task 5
+
+var arrayone = [1, 4, 'Iwona', false, 'Nowak'];
+var firstname = arrayone[2],
+    lastname = arrayone[4];
